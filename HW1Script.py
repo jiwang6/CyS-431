a73e1113600e05eb7980090620bd0dfcb6e710ba
Jim <jim.z.wang8@gmail.com>
2022-01-12 11:10:48 -0700

    hw-1 complete

---
+++ HW1Script.py
@@ -0,0 +1,29 @@
+import re
+
+with open('testFiles/hw1.txt', 'r') as file:
+    rawString = file.read().replace('\n', '')
+
+rawString = rawString.replace(' ', '')
+rawString = rawString.lower()
+rawString = re.sub(r'[^\w\s]','',rawString)
+
+def frequency(txt, sign):
+    counter: int = 0
+    for s in txt:
+        if s != sign:
+            continue
+        counter += 1
+    return counter
+
+valDict = {}
+
+for s in 'abcdefghijklmnopqrstuvwxyz':
+    howMany = frequency(rawString, s)
+    percent = 100 * howMany / len(rawString)
+    valDict.update({s:percent})
+
+valDict = {k: v for k, v in sorted(valDict.items(), key=lambda item: item[1], reverse=True)}
+
+for key, val in valDict.items():
+    val = str(round(val, 2))
+    print('\'' + key + '\'' + ' - ' + val + '%')
