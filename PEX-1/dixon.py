b3b4353a89f000266ba9b51142df785e20edde28
Jim <jim.z.wang8@gmail.com>
2022-04-06 19:25:05 -0600

    broke

--- PEX-1/dixon.py
+++ PEX-1/dixon.py
@@ -1,16 +1,43 @@
 import numpy as np
 from primegen import gen_primes
+import random
 
 
 def dixon(n,t=5):
 	
 	base = gen_primes(t)
-	print(f"Done generating factor base: {base}")
+	print("Done generating factor base.")
 
-	for i in range(1,t+1):
-		print(f"{i} ")
+	good_LHS =[]
+	
+	for _ in range(t):
+		new_int = random.randint(2,n-1)
+		while new_int in good_LHS: # prevents dup
+			new_int = random.randint(2,n-1)
+		good_LHS.append(new_int)
+
+	good_RHS = [pow(i,2,n) for i in good_LHS]
+
+	print(base)
+	print(good_LHS)
+	print(good_RHS)
+
+	g_matrix = np.zeros(t)
+
+	for rhs in good_RHS:
+		EV = np.zeros(t)
+
+		for b_index in range(t):
+			while rhs % base[b_index] == 0:
+				rhs /= base[b_index]
+				EV[b_index] +=1
+		print(EV)
+
+
+	print(g_matrix)
+	print(good_RHS)
 
 
 if __name__ == "__main__":
-    print("fuck")
+	dixon(187, 10)
     
