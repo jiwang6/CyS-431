d5b5cedd3ce1bbbb7fcdba8de4700570a934fbb2
Jim <jim.z.wang8@gmail.com>
2022-04-28 11:04:24 -0600

    added stuff

--- PEX-2/hashpy.py
+++ PEX-2/hashpy.py
@@ -11,22 +11,29 @@ def hash_str(string):
 
 def find_collison(test_bin, tiny_val, reset = True, hash_len = 5, 
 					max_length = 600000):
+	
+	# save original binary
 	original_bin = test_bin
 
+	# init params
 	num_col = 0
 	bytes_tried = 0
 	successful_try = 0
 
+	# main driver loop
 	while True:
-		for j in range(0xff): # try everything
+		for j in range(0xff): # try every binary value from 0 to 255
 			test_val = bytes([j])
-			temp_bin = test_bin + test_val
-			test_tiny = hash_str(temp_bin)[:hash_len]
+			temp_bin = test_bin + test_val # append bin
+			test_tiny = hash_str(temp_bin)[:hash_len] # hash
 			bytes_tried += 1
 
-			if test_tiny == tiny_val: # if collision, do stuff
+			#print(test_tiny)
+
+			if test_tiny == tiny_val: # if collision, save to files, set params
 				print(f"Collision #{num_col+1} found after {bytes_tried} " + 
 					f"bytes (size: {bytes_tried-successful_try} bytes)")
+	
 				successful_try = bytes_tried
 				num_col += 1
 
@@ -35,33 +42,31 @@ def find_collison(test_bin, tiny_val, reset = True, hash_len = 5,
 				print(f"Collision saved as file: collision{num_col}.txt")
 				if num_col == 5:
 					return
-				if reset:
+				if reset: # reset search
 					test_bin = original_bin
 
-		if bytes_tried - successful_try > max_length: # reset if too many try
+		if bytes_tried - successful_try > max_length: # reset if too many tries
 			test_bin = original_bin
 			print(f"No hash found for {max_length} hashes,"
 				+ " resetting binary")
 			successful_try = bytes_tried
 		
-		test_bin += bytes([random.randint(0, 255)]) # add random byte
+		test_bin += bytes([random.randint(0, 255)]) # add random after iters
 
-def cheat_alice(tiny_val): # we probably need to iterate through prices
+def cheat_alice(tiny_val): 
 	alt_contents = ("This is a contract between Alice and Bob. Alice agrees "
 		+ "to sell to Bob her NFT art for a price of: $")
-	
 
 	alt_contents = bytes(alt_contents, "utf-8")
 
-	orig = alt_contents
+	orig = alt_contents # save original
 
-	for price in range(100000):
+	for price in range(100000): # try all prices
 		alt_contents += bytes(str(price), "utf-8")
 
 		alt_hash = hash_str(alt_contents)[:5]
 
-		#print(f"trying price: {price}\n hash: {alt_hash}, {tiny_hash}")
-		if alt_hash[:5] == tiny_val:
+		if alt_hash[:5] == tiny_val: # if match collision, save to file
 			# alt_contents to file
 			print(f"Found TinyHash Collision using this number: {price}")
 			print("New contract saved to file: newcontract.txt")
@@ -71,7 +76,7 @@ def cheat_alice(tiny_val): # we probably need to iterate through prices
 
 		alt_contents = orig
 		
-	return -1
+	return -1 # fail
 
 
 if __name__ == "__main__":
--- PEX-2/main.py
+++ PEX-2/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# ----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Created By  : C1C Jim Wang
 # Created Date: 27 April
 # Documentation: none.
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # import modules
 from hashpy import hash_str, find_collison, read_bin, cheat_alice
@@ -14,9 +14,10 @@ print("CyS 431 PEX2 - Hash Collider - by C/Wang")
 
 print("\nStarting Task 1...\n")
 
-hash_len = 5
+hash_len = 5 # tinyhash length
 
-filename = "src-files/samplefile.txt"	
+# read file binary and calculate tinyhash
+filename = "src-files/samplefile.txt" 
 file_bin = read_bin(filename)
 md5_full = hash_str(file_bin)
 tiny_hash = md5_full[:hash_len]
@@ -25,18 +26,19 @@ print(f"Running hash collision for file: {filename}")
 print(f"Full MD5 digest is: {md5_full}")
 print(f"TinyHash digest is: {tiny_hash}")
 
-random_gen = "y"
-reset_val = "y"
+# max hashed tried before resetting binary
 max_len = 600000
 
-find_collison(file_bin, tiny_hash, random_gen, hash_len, max_len)
+# run collider
+find_collison(file_bin, tiny_hash)
 
 print("\nStarting task 2...\n")
 print("Running Hash Collision for file: contract.txt")
+
+# read file binary and calculate tinyhash for NFTs
 cont_hash = hash_str(read_bin('src-files/contract.txt'))
 print(f"Full MD5 digest is: {cont_hash}")
 print(f"TinyHash digest is: {cont_hash[:5]}")
 
-
-
+#run collider
 cheat_alice(cont_hash[:5])
